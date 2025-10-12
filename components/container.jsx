22417cfefab6228af5665d67943e49270ad44dcb
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-10-12 11:20:40 +0530

    Update container.jsx

--- components/container.jsx
+++ components/container.jsx
@@ -1,3 +1,5 @@
+// This component is related to container related
+
 export default function Container({ children }) {
   return(
     <div className="px-6 lg:px-14 max-w-screen-3xl mx-auto w-full">
